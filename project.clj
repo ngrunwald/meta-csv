2fb103be33023c316755fc6b6cd58905dd57cc09
Nils Grunwald <nils@grunwald.fr>
2020-03-16 23:42:49 +0100

    switched to deps.edn for dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/ngrunwald/meta-csv"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [de.siegmar/fastcsv "1.0.3"]
-                 [com.ibm.icu/icu4j "65.1"]]
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :repl-options {:init-ns meta-csv.core}
   :profiles {:dev {:dependencies [[metosin/testit "0.4.0"]]}})
